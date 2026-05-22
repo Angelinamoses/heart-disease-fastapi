681ff791f08d47a194214096c5ade15b4a839631
Angelina <angeleena.darla@gmail.com>
2026-05-22 18:01:16 +0530

    Built heart disease prediction FastAPI app

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+
+from schemas import HeartDiseaseInput
+
+
+app = FastAPI(
+    title="Heart Disease Prediction API",
+    description="Predicts heart disease risk using a trained logistic regression model",
+    version="1.0.0"
+)
+
+model = joblib.load("heart_disease_model.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "Heart Disease Prediction API is running"}
+
+
+@app.post("/predict")
+def predict(data: HeartDiseaseInput):
+    input_data = pd.DataFrame([data.model_dump()])
+
+    prediction = model.predict(input_data)[0]
+    probability = model.predict_proba(input_data)[0][1]
+
+    result = "Heart Disease Detected" if prediction == 1 else "No Heart Disease Detected"
+
+    return {
+        "prediction": result,
+        "probability": round(float(probability) * 100, 2)
+    }
